88ffed0e430c4999e019ca175eddcb66edabc507
Anhgelus Morhtuuzh <anhgelus.morhtuuzh@gmx.com>
2022-11-14 21:30:07 +0100

    feat(ui) update style and add responsiveness

--- src/custom-elements/CustomSection.ts
+++ src/custom-elements/CustomSection.ts
@@ -5,6 +5,11 @@ export default class CustomSection extends HTMLElement {
     connectedCallback() {
         const content = this.innerHTML;
         const title = this.getAttribute('title');
-        this.innerHTML = `<section class="section"><h2 class="is-2 title little-padding">${title}</h2>${content}</section>`;
+        const isLarger = this.hasAttribute('is-larger');
+        if (isLarger) {
+            this.innerHTML = `<section class="section"><h2 class="is-2 title little-padding larger">${title}</h2>${content}</section>`;
+        } else {
+            this.innerHTML = `<section class="section"><h2 class="is-2 title little-padding">${title}</h2>${content}</section>`;
+        }
     }
 };
