7305654cc33b2aaeb254138d4462b01da1c6b807
Anhgelus Morhtuuzh <anhgelus.morhtuuzh@gmx.com>
2022-11-16 17:32:33 +0100

    refactor(routes) routing system

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import about from "./views/about";
 
 export const routes = [
     {
-        path: '/',
+        path: '',
         component: home
     },
     {
@@ -14,7 +14,7 @@ export const routes = [
         component: page404
     },
     {
-        path: '/about',
+        path: 'about',
         component: about
     }
 ]
@@ -22,21 +22,24 @@ export const routes = [
 registerCustomElement();
 
 export const appDiv = document.getElementById('app');
+const path = window.location.pathname.substring(1);
 
 let is404 = true;
 routes.forEach(route => {
-    if (window.location.pathname == route.path) {
+    if (route.path.startsWith(path) && path !== '') {
         route.component();
         is404 = false;
     }
 });
 
-if (is404) {
+if (is404 && path !== '') {
     const page404 = routes.find(route => route.path == '404');
     if (page404 === undefined) {
         throw new Error('404 page not found');
     }
     page404.component();
+} else if (path === '') {
+    home();
 }
 
 const externalLinks = document.querySelectorAll('a.is-external');
