2b13e4ba25f7d7b8a330eb0f16e04316b642fa66
Anhgelus Morhtuuzh <anhgelus.morhtuuzh@gmx.com>
2022-12-07 16:52:02 +0100

    refactor(core) switch to ghost

--- src/custom-elements/index.ts
+++
@@ -1,9 +0,0 @@
-import CustomSection from "./CustomSection";
-import NewsCard from "./NewsCard";
-import MarkdownParser from "./MarkdownParser";
-
-export default function registerCustomElement() {
-    customElements.define('custom-section', CustomSection);
-    customElements.define('news-card', NewsCard);
-    customElements.define('markdown-parser', MarkdownParser);
-}
